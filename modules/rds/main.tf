1703e6cca296a7b9a4b0ad246afc0cf23e9382c0
mostafaibrahim24 <mi.mostafaibrahim@gmail.com>
2025-03-02 18:38:43 +0200

    Add DB resources

---
+++ modules/rds/main.tf
@@ -0,0 +1,59 @@
+data "aws_subnet" "db-private-subnet1"{
+    filter {
+      name = "tag:Name"
+      values = [var.db-private-subnet1]
+    }
+}
+data "aws_subnet" "db-private-subnet2"{
+    filter {
+      name = "tag:Name"
+      values = [var.db-private-subnet2]
+    }
+}
+data "aws_security_group" "db-tier-sg"{
+    filter {
+      name = "tag:Name"
+      values = [var.db-tier-sg-name]
+    }
+}
+resource "aws_db_subnet_group" "db-tier-subnet-gp" {
+  name = var.db-tier-subnet-gp-name
+  subnet_ids = [data.aws_subnet.db-private-subnet1,data.aws_subnet.db-private-subnet2]
+}
+resource "aws_rds_cluster" "aurora-cluster" {
+  cluster_identifier = "aurora-cluster"
+  engine                  = "aurora-mysql"
+  engine_version          = "8.0.mysql_aurora.3.02.2"
+  master_username         = var.rds-username
+  master_password         = var.rds-pwd #no worries, value won't actually be exposed that way (way of passing it)
+  backup_retention_period = 7
+  preferred_backup_window = "07:00-09:00"
+  skip_final_snapshot     = true
+  database_name           = var.db-name
+  port                    = 3306
+  db_subnet_group_name = aws_db_subnet_group.db-tier-subnet-gp.name
+  vpc_security_group_ids = [data.aws_security_group.db-tier-sg]
+  tags = {
+    Name = var.rds-cluster-name
+  }
+}
+
+# RDS Primary instance
+resource "aws_rds_cluster_instance" "primary-instance" {
+  cluster_identifier = aws_rds_cluster.aurora-cluster.id
+  identifier         = "primary-instance"
+  instance_class     = "db.r5.large"
+  engine             = aws_rds_cluster.aurora-cluster.engine
+  engine_version     = aws_rds_cluster.aurora-cluster.engine_version
+}
+
+# RDS Read Replica Instance
+resource "aws_rds_cluster_instance" "read-replica-instance" {
+  count              = 1
+  cluster_identifier = aws_rds_cluster.aurora-cluster.id
+  identifier         = "read-replica-instance-${count.index}"
+  instance_class     = "db.r5.large"
+  engine             = aws_rds_cluster.aurora-cluster.engine
+
+  depends_on = [aws_rds_cluster_instance.primary-instance]
+}
---
+++ modules/rds/variables.tf
@@ -0,0 +1,23 @@
+
+variable "db-private-subnet1"{}
+
+variable "db-private-subnet2"{}
+
+variable "db-tier-sg-name" {
+  
+}
+variable "db-tier-subnet-gp-name" {
+  
+}
+variable "rds-username" {
+  
+}
+variable "rds-pwd" {
+  
+}
+variable "db-name" {
+  
+}
+variable "rds-cluster-name" {
+  
+}
